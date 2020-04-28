04522b228a2e5793268dec4182e362c36228a877
Saintmember <saintmember@ukr.net>
2020-04-28 10:54:53 +0300

    add Slingshot

---
+++ Assets/Scripts/SlingShot.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShot : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+    public float velocityMult = 8f;
+    [Header("Set Dynamically")]
+
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile; // b
+    public bool aimingMode;
+    private Rigidbody projectileRigidbody;
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false); // b
+        launchPos = launchPointTrans.position;
+    }
+    // Start is called before the first frame update
+    void OnMouseEnter()
+    {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false); //    
+    }
+    void OnMouseDown()
+    { // d
+      // Игрок нажал кнопку мыши, когда указатель находился над рогаткой
+        aimingMode = true;
+        // Создать снаряд
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // Поместить в точку launchPoint
+        projectile.transform.position = launchPos;
+        // Сделать его кинематическим
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!aimingMode) return;
+        Vector3 mousePos2D = Input.mousePosition; // с
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        // Ограничить mouseDelta радиусом коллайдера объекта Slingshot // d
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButtonUp(0))
+        { // e
+          // Кнопка мыши отпущена
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
+    }
+}
